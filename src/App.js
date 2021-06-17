9f682f35c05992464fa92c3f1851507a460f6309
Abdur-Rasheed <abdurrasheedsyed512@gmail.com>
2021-06-16 22:44:11 -0400

    I have made the campus part which will allows us to display the Name, picture, description, and address of the campus

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import CampusInfo from './components/CampusInfo'
+
+
+
+function App(){
+return(
+  <div>
+    <h3>Hello World</h3>
+    <CampusInfo/>
+  </div>
+
+);
+}
+export default App;
